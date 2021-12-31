258c59059c3ed2e144f3fd91b1982680b6fcd314
DZ <78488493+DDAEZI@users.noreply.github.com>
2021-12-31 23:34:00 +0900

    4주차 HW
    index.html : 수량 -> 수령 방법으로 수정, Post/Get함수 추가
    app.py -> 주문 저장 및 주문 목록 보기

---
+++ 주차별 과제/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.dbhomework
+
+## HTML 화면 보여주기
+@app.route('/')
+def homework():
+    return render_template('index.html')
+
+# 주문하기(POST) API
+# 주문자 이름, 상품 종류, 수령 방법, 주소, 전화번호
+@app.route('/order', methods=['POST'])
+def save_order():
+    name_receive = request.form['name_give']
+    type_receive = request.form['type_give']
+    ship_receive = request.form['ship_give']
+    address_receive = request.form['address_give']
+    phone_receive = request.form['phone_give']
+
+    doc = {
+        'name':name_receive,
+        'type':type_receive,
+        'ship':ship_receive,
+        'address':address_receive,
+        'phone':phone_receive
+    }
+    db.order.insert_one(doc)
+    return jsonify({'msg': '주문되었습니다! 감사합니다! :)'})
+
+# 주문 목록보기(Read) API
+@app.route('/order', methods=['GET'])
+def view_orders():
+    orders = list(db.order.find({},{'_id':False}))
+    return jsonify({'all_orders':orders})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=50001, debug=True)
